b7235593f01757b95daaf1bb187d0124fac195be
YugeshKeluskar <yugeshkeluskar.yk@gmail.com>
2024-01-01 19:16:53 +0530

    123

--- HelloWorld/src/components/app.tsx
+++ HelloWorld/src/components/app.tsx
@@ -24,13 +24,15 @@ export const App = registerCustomElement(
     useEffect(() => {
       Context.getPageContext().getBusyContext().applicationBootstrapComplete();
     }, []);
-    
+
     return (
       <div id="appContainer" class="oj-web-applayout-page">
         <Header
-          appName={appName} 
-          userLogin={userLogin} 
+          appName={appName}
+          userLogin={userLogin}
         />
+
+        
         <Content />
         <Footer />
       </div>
